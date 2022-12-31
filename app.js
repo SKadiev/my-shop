8f5e36ed19ec42d8af826d3c0ca8decfc5c45122
SKadiev <kadievstojan@gmail.com>
2022-12-31 16:21:54 +0100

    Setup ejs with routes

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path')
+const bodyParser = require('body-parser');
 const app = express();
+const indexRoutes = require('./routes/index');
 app.set('view engine', 'ejs')
-
 app.set('views', 'views');
-app.use(express.static('public'))
-app.get('/', (req, res, next) => {
-  return res.render('pages/index', { title: 'Index page' });
-});
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(indexRoutes);
+
+app.get('*', (req, res, next) => {
+  res.status(404).render('pages/404', { title: 'Page not found' });
+})
 
 app.listen(8000, () => {
   console.log('Listening on port 8000');
