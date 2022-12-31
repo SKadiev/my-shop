be31f02da620f24a8bec64264850d711ed1cbdd9
SKadiev <kadievstojan@gmail.com>
2022-12-31 13:58:11 +0100

    Set up static file serving

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const path = require('path')
 const app = express();
-
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res, next) => {
-  return res.send('<h1>Test</h1>');
+  return res.send('<link href="/styles.css" ><h1>Test</h1>');
 });
 
 app.listen(8000, () => {
