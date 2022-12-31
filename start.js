00f5e52e1671c64b91192996e1b4a35c25eb6cc7
SKadiev <kadievstojan@gmail.com>
2022-12-31 21:49:02 +0100

    Establish connection to the mongodb database

--- start.js
+++ start.js
@@ -1,7 +1,24 @@
+//* Start server
+
 require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.Database, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection
+  .on('open', () => {
+    console.log('Mongoose connection open');
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
 
 const app = require('./app');
 
 const server = app.listen(8000, () => {
   console.log('Listening on port 8000');
 })
+
